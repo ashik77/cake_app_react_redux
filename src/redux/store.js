b11323c90c85a83df213ee6c1ba4add494b0de52
MD ASHIQUR RAHMAN <ashikaman77@gmail.com>
2022-08-28 18:10:23 +0300

    final commit

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,6 @@
-import { configureStore } from "redux";
+import { createStore } from "redux";
 import cakeReducer from "./cake/cakeReducer";
 
-const store = configureStore(cakeReducer);
+const store = createStore(cakeReducer);
 
 export default store;
