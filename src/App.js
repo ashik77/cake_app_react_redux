a5a9d0b5489686b6a3dc8a09c94ae1878d654aee
MD ASHIQUR RAHMAN <ashikaman77@gmail.com>
2022-08-31 00:35:18 +0300

    async actions with redux

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ import CakeContainer from "./components/CakeContainer";
 import HooksCakeContainer from "./components/HooksCakeContainer";
 import IceCreamContainer from "./components/IceCreamContainer";
 import NewCakeContainer from "./components/NewCakeContainer";
+import UserContainer from "./components/UserContainer";
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
+        <UserContainer />
         <HooksCakeContainer />
         <CakeContainer />
         <IceCreamContainer />
--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import CakeReducer from "./cake/cakeReducer";
 import iceCreamReducer from "./iceCream/iceCreamReducer";
+import userReducer from "./user/userReducer";
 
 const rootReducer = combineReducers({
   cake: CakeReducer,
   iceCream: iceCreamReducer,
+  user: userReducer,
 });
 
 export default rootReducer;
